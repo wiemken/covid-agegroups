525b3925ca33843d4ac07492897f2c03d9b4824d
Samson Niemotka <sniemotka2@gmail.com>
2021-08-31 14:09:03 -0500

    Fixed breaks

--- covid_agegroups/shiny_helpers.R
+++ covid_agegroups/shiny_helpers.R
@@ -173,7 +173,8 @@ plotter <- function(data, corrected = F, age_groups, color = F){
             geom_line(size=0.8) +
             ylab("Number of Cases \n") +
             xlab("\nWeek Ending") +
-            scale_y_continuous(label=comma, limits=c(0,(max(data$cases))+.05*max(data$cases)), breaks=c(seq(0,max(data$cases), by=25000))) +
+            scale_y_continuous(label=comma, limits=c(0,max(data$cases)), breaks=c(seq(0, (max(data$cases)+(1000-max(data$cases)%%1000)),
+                                                                                      by = (max(data$cases)+(1000-max(data$cases)%%1000))/10))) +
             scale_linetype_manual(name = "", values = c("solid", "longdash", "dotted","solid", "longdash", "dotted","solid", "longdash", "dotted","solid")) + 
             scale_colour_manual(name = "Age Group", values = c("0-4 Years" = "#e9cb67", "5-11 Years" = "#e3bb42", "12-15 Years" = "#dbaa2e",
                                         "16-17 Years" = "#937926", "18-29 Years" = "#00adad", "30-39 Years" = "#007574", "40-49 Years" = "#004848", "50-64 Years" = "#fcabd5",
@@ -206,7 +207,8 @@ plotter <- function(data, corrected = F, age_groups, color = F){
             geom_line(size=0.8) +
             ylab("Number of Cases \n") +
             xlab("\nWeek Ending") +
-            scale_y_continuous(label=comma, limits=c(0,(max(data$cases))+.05*max(data$cases)), breaks=c(seq(0,max(data$cases), by=25000))) +
+            scale_y_continuous(label=comma, limits=c(0,max(data$cases)), breaks=c(seq(0, (max(data$cases)+(1000-max(data$cases)%%1000)),
+                                                                                      by = (max(data$cases)+(1000-max(data$cases)%%1000))/10))) +
             scale_linetype_manual(name = "", values = c("solid", "longdash", "dotted","solid", "longdash", "dotted","solid", "longdash", "dotted","solid")) + 
             scale_colour_manual(name = "Age Group", values = c("0-4 Years" = "#BEBEBE", "5-11 Years" = "#C0C0C0", "12-15 Years" = "#C8C8C8",
                                            "16-17 Years" = "#D0D0D0", "18-29 Years" = "#D3D3D3", "30-39 Years" = "#D8D8D8", "40-49 Years" = "#DCDCDC", "50-64 Years" = "#E0E0E0",
@@ -225,7 +227,7 @@ plotter <- function(data, corrected = F, age_groups, color = F){
       }
 }
 
-#plotter(df, age_groups = "All", color = F, corrected = T)
+plotter(df, age_groups = "All", color = T, corrected = T)
 
 
 ### start plotting first three age groups
