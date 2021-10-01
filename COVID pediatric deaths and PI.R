a692d49c246f7932a645439e175b363766cca6c3
lepromatous <tim.wiemken@gmail.com>
2021-10-01 16:57:13 -0500

    added computations in covid pediatric deaths for flu hospitalization

--- COVID pediatric deaths and PI.R
+++ COVID pediatric deaths and PI.R
@@ -15,4 +15,32 @@ df %>%
   filter(state == "United States" & 
            sex == "All Sexes" & 
            age_group%in%c("Under 1 year", "1-4 years", "5-14 years")) -> df
+
+
+
+### flu hospitalizations:  https://gis.cdc.gov/GRASP/Fluview/FluHospRates.html
+df <- vroom("https://raw.githubusercontent.com/wiemken/covid-agegroups/main/FluSurveillance_Custom_Download_Data.csv")
+
+df %>%
+  janitor::clean_names() -> df
+
+table(df$age_category)
+df %>%
+  filter(catchment == "Entire Network" & 
+           network == "FluSurv-NET" & 
+           age_category == "< 18" &
+           sex_category == "Overall" &
+           race_category == "Overall") -> df
+
+pop <- 72822113
+
+df <- data.frame(df)
+df <- df[-c(1:5),]
+df <- subset(df, df$mmwr_week!=53)
+row.names(df) <- seq(1:nrow(df))
+
+df$cases <- round(as.numeric(df$cumulative_rate)*pop/100000,0)
+keep <- seq(0,360, by=30)[-1]
+median(df[keep,"cases"], na.rm=T)
   
+        
