80a9acc97b11a434f117ed88c972a19a8f74bb5c
Wiemken <WIEMKT@pfizer.com>
2021-10-11 16:19:28 -0500

    updating app to add hospitalizations

--- covid_agegroups/app.R
+++ covid_agegroups/app.R
@@ -7,13 +7,16 @@ library(shiny)
 library(DT)
 source("shiny_helpers.R")
 source("load_data.R")
+source("load_data_hospitalization.R")
 
 ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(width = 3,
-            strong("Estimating COVID-19 Case Counts Over Time"),
+            strong("Estimating COVID-19 Case Counts and Hospitalization Counts Over Time"),
             hr(),
-            radioButtons(inputId = "pctchange", label = "Plot type?", choices = c("Cases" = 1, "Percentage Change" = 2, "Rate Per 100,000" = 3)),
+            radioButtons(inputId = "plottype", label = "Plot type?", choices = c("Cases" = 1, "Percentage Change in Cases" = 2, "Rate Per 100,000 (Cases)" = 3,
+                                                                                    "Hospitalizations" = 4, "Percentage Change in Hospitalizations" = 5, "Rate Per 100,000 (Hospitalizations)" = 6)
+                         ),
             radioButtons(inputId = "bwcolor", label = "Plot color scheme?", choices = c("Color" = T, "Black and White" = F)),
             selectizeInput(inputId = "age_group_selector", label = "Age? (Select all that apply, selecting 'All' will override all other choices",
                         choices = c("All", levels(df_shiny$age_group)), selected = levels(df_shiny$age_group)[1],
@@ -36,14 +39,23 @@ server <- function(input, output) {
 
     output$plot_output <- renderUI({
         tagList(
-        if(input$pctchange == 1){
+        if(input$plottype == 1){
             renderPlotly(plotter(df_shiny, age_groups = input$age_group_selector, color = input$bwcolor, corrected = input$correctyn))
         },
-        if(input$pctchange == 2){
+        if(input$plottype == 2){
             renderPlotly(plotterpct(df_shiny, age_groups = input$age_group_selector, color = input$bwcolor, corrected = input$correctyn))
         },
-        if(input$pctchange == 3){
+        if(input$plottype == 3){
             renderPlotly(plotterrate(df_shiny_rate, age_groups = input$age_group_selector, color = input$bwcolor))
+        },
+        if(input$plottype == 4){
+            renderPlotly(plotterrate(df_shiny_rate_hosp, age_groups = input$age_group_selector, color = input$bwcolor))
+        },
+        if(input$plottype == 5){
+            renderPlotly(plotterrate(df_shiny_rate_hosp, age_groups = input$age_group_selector, color = input$bwcolor))
+        },
+        if(input$plottype == 6){
+            renderPlotly(plotterrate(df_shiny_rate_hosp, age_groups = input$age_group_selector, color = input$bwcolor))
         }
         )
     })
